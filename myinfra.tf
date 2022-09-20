ea7733abe72ed55fec43a6a009899b7185126436
Leon L. Robinson <leon.robinson@qa.com>
2022-09-20 13:30:21 +0100

    Input variable, and output

--- myinfra.tf
+++ myinfra.tf
@@ -10,11 +10,15 @@ provider "aws" {
     region = "eu-west-2"
 }
 
+
 resource "aws_instance" "tf-instance" {
-    ami           = "ami-0fb391cce7a602d1f"
+    ami           = "${var.ami_id}"
     instance_type = "t2.micro"
     key_name      = "22Julyenable1-Lon"
     # security group?
     security_groups = [ "AutoScaling-Security-Group-1" ]
 }
 
+output "PublicIP" {
+    value = aws_instance.tf-instance.public_ip
+}
