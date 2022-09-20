1d5a25d8014b5de0845a9ff08be8340d608ccc72
Leon L. Robinson <leon.robinson@qa.com>
2022-09-20 11:50:51 +0100

    add instance definition

--- myinfra.tf
+++ myinfra.tf
@@ -10,4 +10,11 @@ provider "aws" {
     region = "eu-west-2"
 }
 
+resource "aws_instance" "tf-instance" {
+    ami           = "ami-0fb391cce7a602d1f"
+    instance_type = "t2.micro"
+    key_name      = "22Julyenable1-Lon"
+    # security group?
+    security_groups = [ "AutoScaling-Security-Group-1" ]
+}
 
