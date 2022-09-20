453912b4df29a8864848fcca5e30e019b51241b9
Leon L. Robinson <leon.robinson@qa.com>
2022-09-20 15:51:02 +0100

    added security group

--- awsinstance/main.tf
+++ awsinstance/main.tf
@@ -2,4 +2,5 @@ resource "aws_instance" "tf-instance" {
   ami           = "${var.ami_id}"
   instance_type = "t2.micro"
   key_name      = "22Julyenable1-Lon"
+  security_groups = ["AutoScaling-Security-Group-1"]
 }
